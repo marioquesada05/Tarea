e55b44e8a70fe5137ec22a20a28697edd98eddae
marioquesada05 <mario.quesada@escuelaestech.es>
2024-12-14 00:12:23 +0100

    Add files via upload

---
+++ Tarea2/main.py
@@ -0,0 +1,19 @@
+from collections.abc import Awaitable
+
+
+
+
+
+
+
+
+
+
+a = 5
+b = 3
+if a > b:
+  print(f"El número mayor es { 5 }")
+elif b > a:
+  print(f"El número mayor es { 3 }")
+else:
+  print("Ambos números son iguales.")
